2a072c7f6a223d1194d5d02a04fecf6388dfae29
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-16 13:36:48 -0700

    add show, edit, delete to quzzes controller creat, show, edit, delete views

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,7 @@
 class QuizzesController < ApplicationController
-  
+  before_action :find_quiz, only: [:show, :edit, :update, :destroy]
+
+
   def new
     @quiz = Quiz.new
   end
@@ -14,9 +16,37 @@ class QuizzesController < ApplicationController
     end
 
   end
+  
+  def show
+  end
 
   def index
     @quizzes = Quiz.order(created_at: :desc)
   end
+
+  def edit
+  end
+
+  def update
+    if @quiz.update(quiz_params)
+      redirect_to quiz_path(@quiz)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @quiz.destroy
+    redirect_to quizzes_path
+  end
+
+  private
+  def quiz_params
+    params.require(:quiz).permit(:title,:points,:passmark)
+  end
+  
+  def find_quiz
+    @quiz = Quiz.find(params[:id])
+    end
 
 end
