ba3ccc317124a10a67ee8ab6ba3a67709f725102
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-16 14:49:40 -0700

    creat user model

---
+++ db/migrate/20180616214457_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+      t.boolean :approved
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
