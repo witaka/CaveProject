4a90de157e281c2c633ce86c7f109f328b1a193b
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-16 12:56:06 -0700

    add new,create,index to quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,22 @@
 class QuizzesController < ApplicationController
+  
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+
+    if @quiz.save
+      redirect_to quiz_path(@quiz)
+    else
+      render :new
+    end
+
+  end
+
+  def index
+    @quizzes = Quiz.order(created_at: :desc)
+  end
+
 end
