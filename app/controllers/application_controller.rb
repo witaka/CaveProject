395065b4302681df916b691f5ac8efc867ffd25b
Anthony Sylvester <anthony.sylvester99@gmail.com>
2018-06-16 20:59:48 -0700

    user can sign in, log out and sign up (#24)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find session[:user_id]
+    if session[:user_id].present?
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
