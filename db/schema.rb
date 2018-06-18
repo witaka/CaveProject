2ae126db6740b9e09526fbd727eff9a39e760ef6
Anthony Sylvester <anthony.sylvester99@gmail.com>
2018-06-17 18:33:53 -0700

    fixes badge array on quiz (#36)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_003731) do
+ActiveRecord::Schema.define(version: 2018_06_18_013137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_003731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "level", default: "beginer"
+    t.integer "badges", default: [], array: true
   end
 
   create_table "results", force: :cascade do |t|
